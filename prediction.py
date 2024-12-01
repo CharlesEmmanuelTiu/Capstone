adcaf777c7c869da4508b489b12c2be2306935f7
CharlesEmmanuelTiu <149040116+CharlesEmmanuelTiu@users.noreply.github.com>
2024-12-01 18:08:35 +0300

    Removed the modular and traditional options

--- prediction.py
+++ prediction.py
@@ -1,5 +1,3 @@
-# predict.py
-
 from flask import Flask, jsonify
 from flask_cors import CORS
 import pandas as pd
--- train_model.py
+++ train_model.py
@@ -1,5 +1,3 @@
-# train_model.py
-
 import numpy as np
 import pandas as pd
 import tensorflow as tf
